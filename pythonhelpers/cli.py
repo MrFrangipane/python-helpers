b213523b59746c4c96b1c21a542771566614bc55
MrFrangipane <contact@frangipane.me>
2024-11-25 21:54:33 +0100

    im(cli/): pre compile regex for remove_color_and_style_special_chars

--- pythonhelpers/cli.py
+++ pythonhelpers/cli.py
@@ -1,6 +1,7 @@
 import re
 
+_ANSI_ESCAPE = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+
 
 def remove_color_and_style_special_chars(text):
-    ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-    return ansi_escape.sub('', text)
+    return _ANSI_ESCAPE.sub('', text)
