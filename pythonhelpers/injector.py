d8dfe95c5b30f1d7beaa1c3c56e1fb62836d04ac
MrFrangipane <contact@frangipane.me>
2025-12-09 14:16:44 +0100

    imp(Injector): Check if subclass on registration. Example and test-ish in __main__

--- pythonhelpers/injector.py
+++ pythonhelpers/injector.py
@@ -13,8 +13,11 @@ class Injector(metaclass=SingletonMetaclass):
         if dependencies is not None:
             self._dependencies = dependencies
 
-    def  set_dependencies(self, dependencies: dict):
+    def set_dependencies(self, dependencies: dict):
         for abstract, implementation in dependencies.items():
+            if not isinstance(implementation, abstract):
+                raise TypeError(f"Expected '{implementation.__class__.__name__}' to be an instance of '{abstract.__name__}'")
+
             self._dependencies[abstract] = implementation
 
     def inject(self, abstract: Type[T]) -> T:
@@ -22,3 +25,38 @@ class Injector(metaclass=SingletonMetaclass):
             raise KeyError(f"Injector has no dependency registered for '{abstract.__name__}'")
 
         return self._dependencies[abstract]
+
+
+# TODO write tests
+# Works as tests and example
+if __name__ == '__main__':
+    from abc import ABC, abstractmethod
+
+
+    class IDependency(ABC):
+        @abstractmethod
+        def some_method(self) -> None:
+            pass
+
+    class Dependency(IDependency):
+
+        def some_method(self) -> None:
+            print("Dependency method called")
+
+    injector = Injector()
+    injector.set_dependencies({
+        IDependency: Dependency()
+    })
+
+    dependency = injector.inject(IDependency)
+    dependency.some_method()
+
+    class LooseClass:
+        def some_method(self) -> None:
+            print("Loose class method called")
+
+    injector.set_dependencies({
+        IDependency: LooseClass()
+    })
+    dependency = injector.inject(IDependency)
+    dependency.some_method()
