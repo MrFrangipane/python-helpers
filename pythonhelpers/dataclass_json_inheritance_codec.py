62ad84082cca71a8147491efa449f94b8f5d9a3b
MrFrangipane <contact@frangipane.me>
2025-12-10 19:19:34 +0100

    fix(DataclassJsonInheritanceCodec): list subclasses recursively

--- pythonhelpers/dataclass_json_inheritance_codec.py
+++ pythonhelpers/dataclass_json_inheritance_codec.py
@@ -6,11 +6,15 @@ T = TypeVar("T")
 class DataclassJsonInheritanceCodec(Generic[T]):
 
     @staticmethod
-    def decode(data: dict[str, Any], cls: Type[T], **kwargs) -> T:
-        result = []
+    def _get_all_subclasses(cls: Type[T]) -> set[Type[T]]:
+        return set(cls.__subclasses__()).union(
+            [s for c in cls.__subclasses__() for s in DataclassJsonInheritanceCodec._get_all_subclasses(c)]
+        )
 
+    @staticmethod
+    def decode(data: dict[str, Any], cls: Type[T], **kwargs) -> T:
         mapping = dict()
-        for subclass in cls.__subclasses__():
+        for subclass in DataclassJsonInheritanceCodec._get_all_subclasses(cls):
             mapping[subclass.__name__] = subclass
 
         return mapping[data["__type__"]].from_dict(data)
